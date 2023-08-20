ca34e6238c34350028309056bda18d1d781dd1df
Kirtiprakash29 <60401063+Kirtiprakash29@users.noreply.github.com>
2023-08-20 11:19:13 +0530

    Add files via upload

---
+++ Python_Basics.py
@@ -0,0 +1,419 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment Operator
+
+# In[6]:
+
+
+a=10
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+a=178
+type(a)
+
+
+# In[9]:
+
+
+a1=23.34
+type(a1)
+
+
+# In[10]:
+
+
+a2 = True
+type(a2)
+
+#Python being case sensitive here. So assignment with 'true' doesn't work.'True'works
+
+
+# Python stores True boolean as 1 and False as 0
+
+# In[7]:
+
+
+c = 4+6j
+type(c)
+
+
+# In[8]:
+
+
+c.imag
+#Real and imaginary part
+
+
+# In[9]:
+
+
+c.real
+
+
+# In[10]:
+
+
+c.conjugate
+
+
+# In[1]:
+
+
+True - False
+
+
+# In[2]:
+
+
+True * False
+
+
+# In[3]:
+
+
+True/False
+
+
+# In[5]:
+
+
+import numpy
+True/False
+
+
+# In[1]:
+
+
+a=10
+
+
+# In[2]:
+
+
+a
+
+
+# In[3]:
+
+
+s=Kir
+
+
+# In[5]:
+
+
+s = 'Kirti'
+
+
+# In[6]:
+
+
+s
+
+
+# In[7]:
+
+
+a+s
+
+
+# In[8]:
+
+
+a = '10'
+
+
+# In[9]:
+
+
+a+s
+
+
+# In[10]:
+
+
+x = input("Enter a number")
+
+
+# In[11]:
+
+
+x
+
+
+# In[12]:
+
+
+type(x)
+
+
+# In[14]:
+
+
+x = int(input("Enter a number "))
+
+
+# In[15]:
+
+
+x
+
+
+# In[16]:
+
+
+type(x)
+
+
+# In[17]:
+
+
+l = ['Kir',2,34,56.78,'Pra']
+
+
+# In[18]:
+
+
+type(l)
+
+
+# In[19]:
+
+
+l[3]
+
+
+# In[20]:
+
+
+l[-2]
+
+
+# In[21]:
+
+
+l[2] = 23
+
+
+# In[22]:
+
+
+l.pop()
+
+
+# In[23]:
+
+
+l
+
+
+# In[25]:
+
+
+l.insert(4,29)
+
+
+# In[26]:
+
+
+l
+
+
+# In[27]:
+
+
+l.insert(8,33)
+
+
+# In[28]:
+
+
+l
+
+
+# In[30]:
+
+
+l.index(33)
+
+
+# In[31]:
+
+
+l.replace(55)
+
+
+# In[1]:
+
+
+10 // 3
+
+
+# In[2]:
+
+
+4//2
+
+
+# In[3]:
+
+
+100//50
+
+
+# In[5]:
+
+
+9==8
+
+
+# In[7]:
+
+
+9=8
+#We see '=' is purely assignment operator whereas '==' is used for comparison if two are equal
+
+
+# In[8]:
+
+
+5 & 6
+
+
+# In[9]:
+
+
+bin(5)
+
+
+# In[10]:
+
+
+bin(6)
+
+
+# In[11]:
+
+
+0b101 & 0b110
+
+
+# In[12]:
+
+
+78 or 65
+
+
+# In[13]:
+
+
+bin(78)
+
+
+# In[14]:
+
+
+bin(65)
+
+
+# In[15]:
+
+
+1001110 or 1000001
+
+
+# In[16]:
+
+
+1000001 or 1001110
+
+
+# In[17]:
+
+
+78 | 65
+
+
+# In[19]:
+
+
+70 // 7
+
+# to obtain quotient - // is used
+
+
+# In[20]:
+
+
+30>>4
+
+
+# In[22]:
+
+
+45>>7
+
+
+# In[23]:
+
+
+bin(45)
+
+
+# In[24]:
+
+
+45>>2
+
+
+# In[25]:
+
+
+30<<4
+
+
+# In[27]:
+
+
+24 and 5
+
+
+# In[28]:
+
+
+24 & 5
+
+
+# In[29]:
+
+
+5 and 6
+
+
+# In[30]:
+
+
+bin(24)
+
+
+# In[31]:
+
+
+bin(5)
+
+
+# In[ ]:
+
+
+
+
